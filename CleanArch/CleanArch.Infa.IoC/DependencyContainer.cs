cbbe4a0970c60158b5732049842b8299ff9f28fe
Konstantin Lubenets <constantine.lubenets@gmail.com>
2019-12-25 18:58:18 +0200

    Configure Precentation layer for Dependency Container

---
+++ CleanArch/CleanArch.Infa.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infa.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services) 
+        {
+            //Aplication Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            //Infra.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
