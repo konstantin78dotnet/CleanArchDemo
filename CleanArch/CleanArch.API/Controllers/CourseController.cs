95e7ef983088b58701fba523b8599fd47f8751e1
Konstantin Lubenets <constantine.lubenets@gmail.com>
2020-01-12 14:29:06 +0200

    Add API Project and Rename controller

---
+++ CleanArch/CleanArch.API/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArch.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
